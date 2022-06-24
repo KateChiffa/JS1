20da64e4683c9385f358e603ab88863e0932234d
Ekaterina <med_golovko@mai.ru>
2022-06-24 14:23:51 +0300

    JS56

---
+++ JS5.js
@@ -0,0 +1,5 @@
+let a = 2
+let c = 10
+for (let b = 1; b<c;b++) 
+{
+console.log(a**b)};
---
+++ JS6.js
@@ -0,0 +1,8 @@
+let a = 2
+let c = 10
+const power = function(c){
+for (let b = 1; b<c;b++) 
+{
+console.log(a**b)}}
+
+power(10)
